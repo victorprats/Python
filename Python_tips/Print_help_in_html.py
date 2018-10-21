761a74beb91ea74cc0d2b6e7713fdc426b81b946
Victor <43514883+victorprats@users.noreply.github.com>
2018-10-21 12:47:50 +0200

    Update Print_help_in_html.py

--- Python_tips/Print_help_in_html.py
+++ Python_tips/Print_help_in_html.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 #
 # --------------------------------------
-#   Print package help in html format
+#   Print package help in html format 
 # --------------------------------------
+# Script Name : Print_help_in_html.py
+# Author : Victor Prats
+# Created : 21 Sep 2018
+# Version : 1.0
+# Description : Print package help in html format 
 
 
 import pydoc
