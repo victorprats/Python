d5ea8d700bdc6714db20e8cc8170373d4c5696a9
Victor <43514883+victorprats@users.noreply.github.com>
2018-12-05 19:43:16 +0100

    Create Comptador_paraules.py

---
+++ Word_counter/Comptador_paraules.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+#
+# --------------------------------------
+#      Comptador de paraules v1.0
+# --------------------------------------
+# Script Name : Comptador_paraules.py
+# Author : Victor Prats
+# Created : 6 Dec 2018
+# Version : 1.0
+# Description : Compta les paraules d'un arxiu txt
+
+
+def inici():
+    # pair of braces creates an empty dictionary: {}
+    # https://docs.python.org/3/tutorial/datastructures.html
+    comptador = {}
+
+    arxiu = input("\nQuin arxiu (*.txt) vols analitzar? ")
+    print("\n - Analitzant document...")
+
+    with open(arxiu, 'r') as document:
+        for line in document:
+            # Treiem caracters de puntuació i passem el text a minúscules
+            llista_paraules = line.replace(',', '').replace('\'', '').replace('/', '').replace('.', '').replace('-', '').\
+                replace('*', '').replace('!', '').lower().split()
+
+            # Fiquem les paraules en una llista
+            for paraula in llista_paraules:
+                # Afegim un contador de cada paraula
+                if paraula not in comptador:
+                    comptador[paraula] = 1
+                else:
+                    # Si la paraula existeix +1 al contador
+                    comptador[paraula] = comptador[paraula] + 1
+
+    # Imprimerix títol de columna en posició determinada
+    print('-' * 30)
+    # print('{:15}{:20}'.format('Paraula', 'Contador'))
+    print("Paraula", "\t\t\tComptador")
+    print('-' * 30)
+
+    # Imprimim les paraules i el comptador.
+    for (paraula, occurencia) in comptador.items():
+        print('{:15}{:10}'.format(paraula, occurencia))
+
+
+# Un altre document?
+def vols_continuar():
+    continua = str(input("\n> Vols analitzar un altre document(s/n)? "))
+
+    if continua == "s":
+        inici()
+    elif continua == "n":
+        exit()
+    else:
+        vols_continuar()
+
+
+if __name__ == '__main__':
+    inici()
+    while True:
+        vols_continuar()
+
