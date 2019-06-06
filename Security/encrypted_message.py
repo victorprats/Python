eb413d712c8025d6d32425597af8f9723caa940c
Victor <victor.prats@live.com>
2019-06-06 16:47:27 +0200

    Create encrypted_message.py

---
+++ Security/encrypted_message.py
@@ -0,0 +1,33 @@
+# ----------------
+# Encrypt message
+# ----------------
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+key = 5
+
+newmsg = ''
+message = input('Enter new message: ')
+
+for character in message:
+    position = alphabet.find(character)
+    newposition = (position+key)%26
+    newchar = alphabet[newposition]
+    print('Encrypted new character is', newchar)
+    newmsg += newchar
+print('The encrypted final message is:', newmsg)
+
+# ----------------
+# Decrypt message
+# ----------------
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+key = 5
+
+newmsg = ''
+message = input('Enter encrypted message: ')
+
+for character in message:
+    position = alphabet.find(character)
+    newposition = (position-key)%26
+    newchar = alphabet[newposition]
+    print('Derypted new character is', newchar)
+    newmsg += newchar
+print('The decrypted final message is:', newmsg)
