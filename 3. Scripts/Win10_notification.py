bd81f2b2b867a0abdcc3a8d6777b91884292bc55
Victor <victor.prats@live.com>
2020-02-05 10:52:58 +0100

    Create Win10_notification.py

---
+++ 3. Scripts/Win10_notification.py
@@ -0,0 +1,6 @@
+#pip install win10toast
+
+from win10toast import ToastNotifier
+
+toaster = ToastNotifier()
+toaster.show_toast("Hello !!!", "This is a notification for you.", icon_path=None, duration=10)
